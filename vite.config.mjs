69a53ff0ce5f3845f12f11806688afb6fbddb7d3
kush <=>
2025-10-25 11:05:48 +0530

    sss

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,16 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // Important: relative paths for assets so Vercel serves them correctly
   base: "./",
-
   build: {
-    outDir: "dist", // your current folder is fine
-    emptyOutDir: true, // clears dist folder before build
-  },
-
-  server: {
-    port: 3000,
+    outDir: "build", // change from 'dist' to 'build'
+    emptyOutDir: true,
   },
 });
