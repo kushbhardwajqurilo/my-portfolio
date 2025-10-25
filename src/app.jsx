f7cd0a8d8a231adff883bc00ac52a6610b960503
kush <=>
2025-10-25 15:10:06 +0530

    backend add

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,47 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './pages/Home'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import ProjectDetail from './pages/ProjectDetail'
-import Blog from './pages/Blog'
-import BlogPost from './pages/BlogPost'
-import Contact from './pages/Contact'
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import ProjectDetail from "./pages/ProjectDetail";
+import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
+import Contact from "./pages/Contact";
 
-export default function App(){
+export default function App() {
+  useEffect(() => {
+    const trackVisitor = async () => {
+      try {
+        await fetch(
+          "https://portfolio-backend-nine-ochre-99.vercel.app/visitor/track-visitor",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      } catch (err) {
+        console.error("Visitor tracking failed:", err);
+      }
+    };
+
+    trackVisitor();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='projects' element={<Projects />} />
-          <Route path='projects/:slug' element={<ProjectDetail />} />
-          <Route path='blog' element={<Blog />} />
-          <Route path='blog/:slug' element={<BlogPost />} />
-          <Route path='contact' element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="projects/:slug" element={<ProjectDetail />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="blog/:slug" element={<BlogPost />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
